f2418f9c6e5e29e11c263b19296b6f4e442778aa
Emerson Silva <emerson_jdss@hotmail.com>
2022-07-30 23:06:39 -0300

    adjust item list

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,50 +1,85 @@
 import { Heading, HStack, Image, Text, VStack } from 'native-base';
-import React from 'react';
+import React, { FC } from 'react';
 import * as S from './styles';
 
-
 const data = [{
-    id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
-    fullName: "SM VITORIA",
-    status: 'active',
-    recentText: "Good Day!",
-    avatarUrl: "https://midias.agazeta.com.br/2021/08/27/navio-brasileiro-pardela-tambem-esta-cumprindo-quarentena-em-vitoria-591319-article.jpg"
+  id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
+  fullName: "SM VITORIA",
+  status: 'active',
+  cruzeVelocit: "12.5",
+  avatarUrl: "https://midias.agazeta.com.br/2021/08/27/navio-brasileiro-pardela-tambem-esta-cumprindo-quarentena-em-vitoria-591319-article.jpg"
+},
+{
+  id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28bc",
+  fullName: "SM VITORIA",
+  status: 'active',
+  cruzeVelocit: "12.5",
+  avatarUrl: "https://midias.agazeta.com.br/2021/08/27/navio-brasileiro-pardela-tambem-esta-cumprindo-quarentena-em-vitoria-591319-article.jpg"
+},
+{
+  id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28bd",
+  fullName: "SM VITORIA",
+  status: 'active',
+  cruzeVelocit: "12.5",
+  avatarUrl: "https://midias.agazeta.com.br/2021/08/27/navio-brasileiro-pardela-tambem-esta-cumprindo-quarentena-em-vitoria-591319-article.jpg"
 },
 ];
 
+const Home: FC = () => {
 
+  return (
+    <VStack
+      p={5}
+      height="80%"
+      mt={10}
+    >
+      <Heading fontSize="xl" p="4" pb="3">
+        Meus Cargueiros
+      </Heading>
 
-const Home: React.FC = () => {
+      {
+        data.map(item => (
+          <HStack w="full"
+            borderColor={'gray.200'}
+            borderWidth={1}
+            p={2}
+            borderRadius={10}
+            key={item.id}
+            mb={5}
+          >
+            <S.ButtonTouch>
+              <Image
+                style={{
+                  borderRadius: 10,
+                }}
+                source={{
+                  uri: item.avatarUrl,
+                }} alt="Alternate Text" size="sm" />
+              <VStack>
+                <Text
+                  ml={5}
+                >
+                  Nome: {item.fullName}
+                </Text>
 
-    return (
-        <VStack
-            p={5}
-            height="80%"
-            mt={10}
-        >
-            <Heading fontSize="xl" p="4" pb="3">
-                Meus Cargueiros
-            </Heading>
+                <Text
+                  ml={5}
+                >
+                  Status: {item.status}
+                </Text>
+                <Text
+                  ml={5}
+                >
+                  Velocidade de Cruzeiro: {item.cruzeVelocit}
+                </Text>
+              </VStack>
 
-            <HStack w="full"
-                borderColor={'gray.500'}
-                borderWidth={1}
-                p={2}
-                borderRadius={10}
-            >
-                <S.ButtonTouch>
-                    <Image source={{
-                        uri: 'https://midias.agazeta.com.br/2021/08/27/navio-brasileiro-pardela-tambem-esta-cumprindo-quarentena-em-vitoria-591319-article.jpg',
-                    }} alt="Alternate Text" size="sm" />
-                    <Text
-                        ml={5}
-                    >
-                        SM VITORIA
-                    </Text>
-                </S.ButtonTouch>
-            </HStack>
-        </VStack >
-    )
+            </S.ButtonTouch>
+          </HStack>
+        ))
+      }
+    </VStack >
+  )
 }
 
 export default Home;
